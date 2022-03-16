d0ece49e96d982ab961ba6bff3b9d58fe538e8c1
Sundram <sundramcodedrill@gmail.com>
2022-03-16 12:08:56 +0530

    FormStructure ScrollFixed

--- src/screens/external/register.js
+++ src/screens/external/register.js
@@ -1,5 +1,5 @@
-import { ScrollView, StyleSheet, TextInput, Text, View, KeyboardAvoidingView, ImageBackground, Image } from 'react-native'
-import { Header, SimpleButton, } from '../../components'
+import { ScrollView, StyleSheet, TextInput, Text, View, KeyboardAvoidingView, ImageBackground, Image, Dimensions } from 'react-native'
+import { Header, SimpleButton, InputBox } from '../../components'
 import { RegisterBackground, Progress, Vector } from '../../assets/Images/index'
 import React, { useState } from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
@@ -14,199 +14,108 @@ const Register = (props) => {
     return (
         // Main View Started
 
-        <KeyboardAvoidingView style={{ flex: 1 }}>
+
+        <ImageBackground source={RegisterBackground} style={{ width: wp("100%"), height: Dimensions.get('window').height }} >
+
             <Header />
             <ScrollView style={{ flex: 1, }} >
-                <ImageBackground source={RegisterBackground} style={{ width: wp("100%"), height: hp("100%") }} >
-                    <View style={{ margin: 15 }}>
-                        <Text style={[styles.textStyle, { fontSize: 28, fontWeight: "500", fontFamily: "Rubik-Medium" }]}>Register</Text>
-                        <Text style={[styles.textStyle, { fontFamily: "Rubik-Regular", fontSize: 14, fontWeight: "100", }]}>Enter all details below to create your account</Text>
-                    </View>
 
-                    {/* Registeration Title View Closed */}
+                {/* Registeration Title View */}
+                <View style={{ margin: 15, }}>
+                    <Text style={[styles.textStyle, { fontSize: 28, fontWeight: "500", fontFamily: "Rubik-Medium" }]}>Register</Text>
+                    <Text style={[styles.textStyle, { fontFamily: "Rubik-Regular", fontSize: 14, fontWeight: "100", }]}>Enter all details below to create your account</Text>
+                </View>
 
-                    <View style={{ flex: 0.2, marginTop: hp("8%"), flexDirection: "row", justifyContent: "center", }}>
-                        <View style={{ flexDirection: "row", }}>
-                            <Text style={styles.stepStyle}> 1 </Text>
-                            {/* RoundCheck  "#717990"  #00C467*/}
-                            <View style={{
-                                width: 16, height: 16, backgroundColor: "#717990", justifyContent: "center", alignItems: "center", position: "absolute", left: wp("7%"), top: hp("0.5%"), borderRadius: 8
-                            }}>
-                                <Image source={Vector} style={{ width: 10, height: 10, }} />
-                            </View>
-                            {/* RoundCheck CLOSED */}
+                {/* Registeration Title View Closed */}
 
-                            {/* Progress Ion + 2 Text */}
-                            <Image source={Progress} style={{ marginTop: 20, marginLeft: 8, marginRight: 8 }} />
-                            <Text style={[styles.stepStyle, { color: "#231D43", borderColor: "#231D43" }]}>2</Text>
-
-                            {/* Progress Ion + 2 Text  CLOSED*/}
+                <View style={{ flex: 1, marginTop: hp("5%"), flexDirection: "row", justifyContent: "center", }}>
+                    <View style={{ flexDirection: "row", }}>
+                        <Text style={styles.stepStyle}> 1 </Text>
+                        {/* RoundCheck  "#717990"  #00C467*/}
+                        <View style={{
+                            width: 16, height: 16, backgroundColor: "#717990", justifyContent: "center", alignItems: "center", position: "absolute", left: wp("7%"), top: hp("0.5%"), borderRadius: 8
+                        }}>
+                            <Image source={Vector} style={{ width: 10, height: 10, }} />
                         </View>
+                        {/* RoundCheck CLOSED */}
+
+                        {/* Progress Ion + 2 Text */}
+                        <Image source={Progress} style={{ marginTop: 20, marginLeft: 8, marginRight: 8 }} />
+                        <Text style={[styles.stepStyle, { color: "#231D43", borderColor: "#231D43" }]}>2</Text>
+
+                        {/* Progress Ion + 2 Text  CLOSED*/}
                     </View>
-
-                    {/* Form Started */}
-                    <View style={{ flex: 1 }}>
-
-                        {/* Personal Details */}
-
-                        <Text style={[styles.textStyle, { fontSize: 16, fontWeight: "500", fontFamily: "Rubik-Medium", marginLeft: 16, marginBottom: 30 }]}>Personal Details</Text>
-
-                        {/* Personal Details Closed */}
+                </View>
 
 
-                        {/* Input Box First Name*/}
-                        <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                            <View style={{ marginLeft: 16 }}>
-                                <TextInput
-                                    style={[styles.firstName, { borderColor: "#707C8B" }]}
-                                    value={data}
-                                    onChangeText={(data) => { setData(data); }}
-                                    onFocus={() => {
-                                        setBorderColorHook("#4E21C9")
-                                    }}
-                                />
-                            </View>
-                            <View style={{ position: "absolute", top: -20, left: wp("8%"), }}>
-                                <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>*First Name</Text>
-                            </View>
+                {/* Personal Details */}
+                <View style={{ marginTop: hp("5%") }}>
+                    <Text style={[styles.textStyle, { fontSize: 16, fontWeight: "500", fontFamily: "Rubik-Medium", marginLeft: 16, marginBottom: 20, marginTop: 48 }]}>Personal Details</Text>
+                </View>
+                {/* Personal Details Closed */}
 
-                        </View >
-                        {/* Input Box  CLOSED Here*/}
-
-                        {/* Input Box Last Name*/}
-                        <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                            <View style={{ marginLeft: 16 }}>
-                                <TextInput
-                                    style={[styles.firstName, { borderColor: "#707C8B" }]}
-                                    value={data}
-                                    onChangeText={(data) => { setData(data) }}
-                                    onFocus={() => {
-                                        setBorderColorHook(true)
-                                    }}
-                                    onBlur={() => { }}
-                                />
-                            </View>
-                            <View style={{ position: "absolute", top: -20, left: wp("8%"), }}>
-                                <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>*Last Name Name</Text>
-                            </View>
-
-                        </View >
-                        {/* Input Box Last Name CLOSED Here*/}
+                {/* Form Started */}
 
 
-                        {/* Input Box UserName*/}
-                        <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                            <View style={{ marginLeft: 16 }}>
-                                <TextInput
-                                    style={[styles.firstName, { borderColor: "#707C8B" }]}
-                                    value={data}
-                                    onChangeText={(data) => { setData(data) }}
-                                    onFocus={() => {
-                                        setBorderColorHook(true)
-                                    }}
-                                    onBlur={() => { }}
-                                />
-                            </View>
-                            <View style={{ position: "absolute", top: -20, left: wp("8%"), }}>
-                                <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>*Username</Text>
-                            </View>
+                {/* Input Box First Name*/}
+                <View style={{ marginBottom: 10 }}>
+                    <InputBox inputTitle="First Name" />
+                </View>
+                {/* Input Box  CLOSED Here*/}
 
-                        </View >
-                        {/* Input Box UserName CLOSED Here*/}
+                {/* Input Box Last Name*/}
+                <View style={{ marginBottom: 10 }}>
+                    <InputBox inputTitle="Last Name" />
+                </View>
+                {/* Input Box Last Name CLOSED Here*/}
 
 
-                        {/* DOB */}
+                {/* Input Box UserName*/}
+                <View style={{ marginBottom: 10, justifyContent: "center", alignItems: "center" }}>
+                    <InputBox inputTitle="User Name" />
+                </View>
+                {/* Input Box UserName CLOSED Here*/}
 
-                        <Text style={[styles.textStyle, { fontSize: 16, fontWeight: "500", fontFamily: "Rubik-Medium", marginLeft: 16, marginBottom: 10 }]}>DOB</Text>
-                        <View style={{ flexDirection: "row", justifyContent: "space-between", marginHorizontal: 16, marginRight: 25 }}>
-                            {/* DD */}
-                            <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                                <View style={{}}>
-                                    <TextInput
-                                        style={[styles.firstName, { borderColor: "#707C8B", width: wp("25%") }]}
-                                        value={data}
-                                        onChangeText={(data) => { setData(data) }}
-                                        onFocus={() => {
-                                            setBorderColorHook(true)
-                                        }}
-                                        onBlur={() => { }}
-                                    />
-                                </View>
-                                <View style={{ position: "absolute", top: -20, left: wp("4%"), }}>
-                                    <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>DD</Text>
-                                </View>
 
-                            </View >
-                            {/* DDCLOSE */}
+                {/* DOB */}
+                <View style={{ marginLeft: 16, marginBottom: 10 }}>
+                    <Text style={[styles.textStyle, { fontSize: 16, fontWeight: "500", fontFamily: "Rubik-Medium" }]}>DOB</Text>
+                </View>
 
-                            {/* MM */}
-                            <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                                <View style={{}}>
-                                    <TextInput
-                                        style={[styles.firstName, { borderColor: "#707C8B", width: wp("27%") }]}
-                                        value={data}
-                                        onChangeText={(data) => { setData(data) }}
-                                        onFocus={() => {
-                                            setBorderColorHook(true)
-                                        }}
-                                        onBlur={() => { }}
-                                    />
-                                </View>
-                                <View style={{ position: "absolute", top: -20, left: wp("4%"), }}>
-                                    <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>MM</Text>
-                                </View>
+                <View style={{ flexDirection: "row", marginLeft: 10 }}>
+                    <InputBox inputTitle="DD" customTextInputStyle={{
+                        width: wp("26%"), marginRight: 0, marginLeft: 0
+                    }} />
+                    <InputBox inputTitle="MM" customTextInputStyle={{ width: wp("26%"), marginLeft: 0, marginRight: 0 }} />
+                    <InputBox inputTitle="YY/YY" customTextInputStyle={{ width: wp("29%"), marginLeft: 0, marginRight: 0 }} />
+                </View>
 
-                            </View >
-                            {/* MMCLOSE */}
+                <View style={{ marginBottom: 10, justifyContent: "center", alignItems: "center" }}>
+                    <InputBox inputTitle="Pssassword" />
+                </View>
 
-                            {/* YY/YY */}
-                            <View style={{ marginTop: 10, padding: 0, marginBottom: 10 }}>
-                                <View style={{}}>
-                                    <TextInput
-                                        style={[styles.firstName, { borderColor: "#707C8B", width: wp("28%") }]}
-                                        value={data}
-                                        onChangeText={(data) => { setData(data) }}
-                                        onFocus={() => {
-                                            setBorderColorHook(true)
-                                        }}
-                                        onBlur={() => { }}
-                                    />
-                                </View>
-                                <View style={{ position: "absolute", top: -20, left: wp("4%"), }}>
-                                    <Text style={{ backgroundColor: "#070417", color: "#707C8B", padding: 5 }}>YY/YY</Text>
-                                </View>
+                <View style={{ marginBottom: 10, justifyContent: "center", alignItems: "center" }}>
+                    <InputBox inputTitle="Confirmssassword" />
+                </View>
 
-                            </View >
-                            {/* YY/YY */}
-                        </View>
-                        {/* DOB CLOSE */}
+                {/* Next Step */}
 
-                        {/* Next Step */}
-                        <SimpleButton title="Next Step" />
 
-                        {/* Next Step CLOSE */}
+
+                {/* Next Step CLOSE */}
 
 
 
 
-                    </View>
 
 
 
-                    <Text style={
-                        [styles.textStyle2, { fontFamily: "Spartan-Medium", color: "#717990", marginTop: hp("15%"), }]
-                    }>
-                        Already Have an account ?
-                        <Text style={[styles.textStyle, { color: "#FFB700" }]} onPress={() => {
-                            props.navigation.navigate("Sign In")
-                        }} >
-                            Register
-                        </Text>
-                    </Text>
-                </ImageBackground>
+
+
             </ScrollView >
-            {/*Main View Ended */}
-        </KeyboardAvoidingView >
+        </ImageBackground>
+
+
 
 
     )
